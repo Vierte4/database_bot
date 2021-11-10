14bfd7571d09ac03018c61704758da930159bf66
Asad Magomedov <84748692+Vierte4@users.noreply.github.com>
2021-11-10 13:23:52 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,7 +1,7 @@
-users = ["316540867", "319503958"]  # Список id всех пользователей
-data_base_path = 'DataBase'  # Имя папки с базой данных
+users = ["", ""]  # Список id всех пользователей
+data_base_path = ''  # Имя папки с базой данных
 files_in_folders = {}
 current_user_paths = {}
 
-ADMINS = ["319503958", ]  # Через запятую в кавычках указывать user id админов
-BOT_TOKEN = '1928480188:AAElLgh83FEcr6RqenSUjPsSzQjD2k1fXxI'
+ADMINS = ["", ]  # Через запятую в кавычках указывать user id админов
+BOT_TOKEN = '' # Ввести токен бота
